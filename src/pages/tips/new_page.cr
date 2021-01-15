9f6c27c98a8e42d49e519dc540331745c41b4588
Jason Hinchcliffe <hinchy@gmail.com>
2021-01-15 14:40:04 +1100

    update blog post and add image folder

--- src/pages/tips/new_page.cr
+++ src/pages/tips/new_page.cr
@@ -5,10 +5,10 @@ class Tips::NewPage < MainLayout
     h1 "Create New Tip"
 
     form_for(Tip::Create) do
-      label_for(@operation.category, "Category")
-      text_input(@operation.category, attrs: [:required])
-      label_for(@operation.description, "Description")
-      text_input(@operation.description, attrs: [:required])
+      label_for(operation.category, "Category")
+      text_input(operation.category, attrs: [:required])
+      label_for(operation.description, "Description")
+      text_input(operation.description, attrs: [:required])
       submit "Create Tip"
     end
   end
